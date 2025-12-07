e831f5cf3bf518b13aa4976a28670ffddcd4dbf3
Skye Gibney <astraliskye@gmail.com>
2025-12-07 08:13:13 -0800

    solution day 7 part 1

--- day7/day7.py
+++ day7/day7.py
@@ -1,19 +1,37 @@
 def parse_input(example=True):
     filename = 'example_input.txt' if example else 'input.txt'
-    content = ''
+    lines = ''
 
     with open(filename) as file:
-        content = file.read()
+        lines = file.readlines()
 
-    # Edit below to parse raw file text into usable data structures
-    output = content
-
-    return output
+    return lines
 
 
 def solve_part1(example=True):
-    input = parse_input(example)
-    pass
+    input_lines = parse_input(example)
+    beam_indices = set([input_lines[0].index('S')])
+    splits = 0
+
+    for line in input_lines[1:]:
+        new_indices = []
+        old_indices = []
+        for beam in beam_indices:
+            if line[beam] == '^':
+                old_indices.append(beam)
+                if beam > 0:
+                    new_indices.append(beam - 1)
+                if beam < len(line) - 1:
+                    new_indices.append(beam + 1)
+                splits += 1
+
+        for new_index in new_indices:
+            beam_indices.add(new_index)
+
+        for old_index in old_indices:
+            beam_indices.remove(old_index)
+
+    return splits
 
 
 def solve_part2(example=True):
--- day7/test_day7.py
+++ day7/test_day7.py
@@ -3,18 +3,14 @@ import day7
 
 
 class TestClass(unittest.TestCase):
-    def test_intermediate_part1(self):
-        pass
-
-    def test_intermediate_part2(self):
-        pass
-
     def test_example_solution_part1(self):
-        pass
+        result = day7.solve_part1()
+        self.assertEqual(result, 21)
 
     def test_example_solution_part2(self):
-        pass
+        result = day7.solve_part2()
+        self.assertEqual(result, 40)
 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     unittest.main()
